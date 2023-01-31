49c94717f6cbbd4bfaf8ef8bbf60e3846b905cf1
NightSkyAsHate <Kazakovgerman@gmail.com>
2023-01-31 21:04:26 +0300

    tasks

---
+++ src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,15 @@
+public class BestTimeToBuyAndSellStock {
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4})); // 5
+        System.out.println(maxProfit(new int[]{7,6,4,3,1})); // 0
+        System.out.println(maxProfit(new int[]{1,4,2})); // 3
+        System.out.println(maxProfit(new int[]{3,2,6,5,0,3})); // 4
+        System.out.println(maxProfit(new int[]{2,7,1,4})); // 5
+        System.out.println(maxProfit(new int[]{7,4,1,2})); // 1
+    }
+
+    public static int maxProfit(int[] prices) {
+
+    }
+}
---
+++ src/CountPairsWithXorInARange.java
@@ -0,0 +1,14 @@
+public class CountPairsWithXorInARange {
+
+    public static void main(String[] args) {
+        System.out.println(countPairs(new int[]{1, 4, 2, 7}, 2, 6));
+        System.out.println(countPairs(new int[]{9, 8, 4, 2, 1}, 5, 14));
+    }
+
+    /**
+     * not completed
+     */
+    public static int countPairs(int[] nums, int low, int high) {
+        return 1;
+    }
+}
